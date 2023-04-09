3054e67128c2ce386aa9e9a1f4041040692593fc
Demkov Boris <demkov.boris@gmail.com>
2023-04-09 13:20:47 +0300

    Added function convert to parquet

--- nb/utilits.py
+++ nb/utilits.py
@@ -3,10 +3,24 @@ import numpy as np
 
 
 def read_initial_csv(path_to_file:str, columns_list):
+    
+    separator = ' '
+    data = pd.read_csv(path_to_file, nrows=1)
+    if len(data.loc[0,columns_list[0]].split(',')) > 1:
+        separator = ','
+    
     return pd.read_csv(
         path_to_file,
         converters=dict.fromkeys(
             columns_list,
-            lambda s: np.fromstring(s[1:-1], sep=' ', dtype=float)
+            lambda s: np.fromstring(s[1:-1], sep=separator, dtype=float)
         )
     )
+
+def save_to_parquet(path_to_file:str, columns_list, path_parquet_file:str):
+    data = read_initial_csv(path_to_file, columns_list)
+    data.to_parquet(
+        path_parquet_file,
+        compression='gzip'
+    )
+    return

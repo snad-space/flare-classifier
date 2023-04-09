a02a748d93948e09c33a6d78006d38b054de4018
Demkov Boris <demkov.boris@gmail.com>
2023-04-10 02:59:46 +0300

    Replace utilits.py to src folder

--- src/flare_classifier/utilits.py
+++ src/flare_classifier/utilits.py
@@ -20,7 +20,6 @@ def read_initial_csv(path_to_file:str, columns_list):
 def save_to_parquet(path_to_file:str, columns_list, path_parquet_file:str):
     data = read_initial_csv(path_to_file, columns_list)
     data.to_parquet(
-        path_parquet_file,
-        compression='gzip'
+        path_parquet_file
     )
     return
